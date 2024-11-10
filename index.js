a328e9a433fea562ff8234ec96ee9f78b9b7f8f6
cconur <98234762+cconur@users.noreply.github.com>
2024-11-10 09:33:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -249,11 +249,11 @@ Highcharts.chart('container', {
         data: [
 
 
-            ['Desarrollos','GSM-DEV-2023',129037,5],
-            ['Desarrollos','P.P-DEV-2023',16687,5],
+            ['Desarrollos','GSM-DEV-2023',129038],
+            ['Desarrollos','P.P-DEV-2023',16688],
             ['Desarrollos','GNM-DEV-2023',24300],
-            ['Desarrollos','SIG-DEV-2023',95737,5],
-
+            ['Desarrollos','SIG-DEV-2023',95738],
+            ['QA-DEV-2022','QA-DEV-2023',0],
 
             ['GSM-DEV-2023','GSM-DEV-2024',183484],
             ['GSM-DEV-2023','GSM-Dist-DEV-2024',76294],
@@ -270,18 +270,19 @@ Highcharts.chart('container', {
             ['SIG-DEV-2024','SIG-DEV-2025',35176],
             ['QA-DEV-2024','QA-DEV-2025',65625],
 
-            ['INN-DEV-2025','GSM-INN-DEV-2026',141000],
             ['INN-DEV-2025','PROY-INN-DEV-2026',20625],
+            //['INN-DEV-2025','GSM-INN-DEV-2026',118500],
+            ['INN-DEV-2025','GSM-COM-DEV-2026',118500],            
             ['COM-DEV-2025','GSM-COM-DEV-2026',223125],
             ['COM-DEV-2025','P.P-COM-DEV-2026',112969],
             ['GNM-DEV-2025','GNM-DEV-2026',216000],
             ['SIG-DEV-2025','SIG-DEV-2026',37500],
             ['QA-DEV-2025','QA-DEV-2026',65625],
 
+            ['GSM-COM-DEV-2026','GSM-INN-DEV-2027',101000],
+            ['GSM-COM-DEV-2026','GSM-INN-DEV-NEW-ARQ-2027',41000],
             ['GSM-COM-DEV-2026','GSM-COM-DEV-2027',190630],
             ['P.P-COM-DEV-2026','P.P-COM-DEV-2027',112969],
-            ['GSM-INN-DEV-2026','GSM-INN-DEV-2027',101000],
-            ['GSM-INN-DEV-2026','GSM-INN-DEV-NEW-ARQ-2027',41000],
             ['PROY-INN-DEV-2026','PROY-INN-DEV-2027',30625],
             ['GNM-DEV-2026','GNM-DEV-2027',150000],
             ['SIG-DEV-2026','SIG-DEV-2027',37500],
@@ -525,41 +526,34 @@ Highcharts.chart('container1', {
         ],
         data: [
 
-
-
             ['Soporte y Mantenimiento','GSM-SUP-2023',218588],
             ['Soporte y Mantenimiento','GNM-SUP-2023',101325],
 
             ['GSM-SUP-2023','GSM-SUP-2024',246263],
             ['GNM-SUP-2023','GNM-SUP-2024',123900],
 
-
             ['Infra-SUP-2024','Infra-SUP-2025',18000],
             ['BI-SUP-2024','BI-SUP-2025',65625],
+            ['SIG-SUP-2024','SIG-SUP-2025',9000],
             ['GSM-SUP-2024','GSM-SUP-2025',196875],
             ['P.P-SUP-2024','P.P-SUP-2025',9000],
             ['GNM-SUP-2024','GNM-SUP-2025',123900],
-            ['SIG-SUP-2024','SIG-SUP-2025',9000],
-
-
 
             ['Infra-SUP-2025','Infra-SUP-2026',18000],
             ['BI-SUP-2025','BI-SUP-2026',65625],
+            ['SIG-SUP-2025','SIG-SUP-2026',5625],
             ['PROY-INN-SUP-2025','PROY-INN-SUP-2026',15000],
             ['GSM-SUP-2025','GSM-SUP-2026',170625],
             ['P.P-SUP-2025','P.P-SUP-2026',28594],
             ['GNM-SUP-2025','GNM-SUP-2026',108750],
-            ['SIG-SUP-2025','SIG-SUP-2026',5625],
-
 
             ['Infra-SUP-2026','Infra-SUP-2027',18000],
             ['BI-SUP-2026','BI-SUP-2027',65625],
+            ['SIG-SUP-2026','SIG-SUP-2027',5625],
             ['PROY-INN-SUP-2026','PROY-INN-SUP-2027',30000],
             ['GSM-SUP-2026','GSM-SUP-2027',180000],
             ['P.P-SUP-2026','P.P-SUP-2027',28594],
             ['GNM-SUP-2026','GNM-SUP-2027',108750],
-            ['SIG-SUP-2026','SIG-SUP-2027',5625],
-   
 
         ],
         type: 'sankey',
